f5743e4bb839baf06666c6e952f67af45bba0d0a
Mack <trenholm.mackenzie@gmail.com>
2020-12-11 01:51:49 -0500

    initial files and blueprint for project

---
+++ test.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+
+trainingData = "covid_training.tsv"
+testData = "covid_test_public.tsv"
+
+with open(trainingData) as f:
+    vocabulary = Counter()
+    next(f)
+    for line in f:
+        #split line into list of strings and lowercase
+        tweet = line.lower().split()
+        #remove first (tweetID) and last 7 (trucs wack) indexes
+        tweet = tweet[1:-7]
+
+        for word in tweet:
+
+            if word in vocabulary:
+                vocabulary[word] += 1
+            else:
+                vocabulary[word] += 1
+
+## print full vocabulary including word with occurance of 1
+print(vocabulary)
+
+##remove all occurences of 1 in vocabulary
+filteredVoc =  Counter(el for el in vocabulary.elements() if vocabulary[el] > 1)
+
+#print updated vocab
+print(filteredVoc)
+
+
+

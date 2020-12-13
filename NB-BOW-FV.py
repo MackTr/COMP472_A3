5c7ee2c9f042dc07ffa97e218540661f11e75560
Alessandro <aleoriskiraly@gmail.com>
2020-12-12 21:19:28 -0500

    Added output files for FV

--- NB-BOW-FV.py
+++ NB-BOW-FV.py
@@ -6,6 +6,8 @@ test_tweets = []
 training_tweets = []
 vocabulary = {}
 features = {'q1': {'yes': {'totalWords': 0, 'probability': 0}, 'no': {'totalWords': 0, 'probability': 0}}}
+name_trace_file = "txt/trace_NB-BOW-FV.txt"
+name_eval_file = "txt/eval_NB-BOW-FV.txt"
 
 results = []
 
@@ -32,7 +34,8 @@ def openFileTestSet():
             # remove first (tweetID) and last 6 indexes
             label = data[-7:-6]
             content = data[1:-7]
-            test_tweets.append({"content": content, "label": label})
+            id = data[0]
+            test_tweets.append({"content": content, "label": label, "id": id})
 
 def getVocabulary():
 
@@ -91,15 +94,9 @@ def testModel():
         score_q1_no = math.log10(features['q1']['no']['probability']) + getScoresForAllWords(tweet['content'], 'no')
 
         if score_q1_yes > score_q1_no:
-            results.append('yes')
+            results.append({'class': 'yes', 'score_yes': score_q1_yes, 'score_no': score_q1_no})
         else:
-            results.append('no')
-
-    for index in range(0, len(results)):
-        if results[index] == test_tweets[index]['label'][0]:
-            print('true')
-        else:
-            print('false')
+            results.append({'class': 'no', 'score_yes': score_q1_yes, 'score_no': score_q1_no})
 
 def filterVocabulary():
     repeat = True
@@ -116,6 +113,84 @@ def filterVocabulary():
                 repeat = True
                 break
 
+def sciNotation(number):
+    return "{:.2e}".format(number)
+
+def printTraceFile():
+    file = open(name_trace_file, 'w')
+    for index in range (0, len(results)):
+        line = test_tweets[index]['id'] + "  "
+        line += results[index]['class'] + "  "
+        line += sciNotation(results[index]['score_' + results[index]['class']]) + "  "
+        line +=  test_tweets[index]['label'][0] + "  "
+        if results[index]['class'] == test_tweets[index]['label'][0]:
+            line += "correct\n"
+        else:
+            line += "wrong\n"
+
+        file.write(line)
+
+def getAccuracy():
+    total = len(test_tweets)
+    correct_values = 0
+    for index in range(0, len(test_tweets)):
+        if results[index]['class'] == test_tweets[index]['label'][0]:
+            correct_values += 1
+    return str(float(correct_values)/total) + '\n'
+
+def getPrecision():
+    total_yes = 0
+    true_yes = 0
+
+    total_no = 0
+    true_no = 0
+
+    for index in range(0, len(test_tweets)):
+        if results[index]['class'] == "yes":
+            total_yes += 1
+            if results[index]['class'] == test_tweets[index]['label'][0]:
+                true_yes += 1
+        else:
+            total_no +=1
+            if results[index]['class'] == test_tweets[index]['label'][0]:
+                true_no += 1
+
+    return float(true_yes)/total_yes, float(true_no)/total_no
+
+def getRecall():
+    total_yes = 0
+    true_yes = 0
+
+    total_no = 0
+    true_no = 0
+
+    for index in range(0, len(test_tweets)):
+        if results[index]['class'] == "yes":
+            if results[index]['class'] == test_tweets[index]['label'][0]:
+                true_yes += 1
+        else:
+            if results[index]['class'] == test_tweets[index]['label'][0]:
+                true_no += 1
+
+    for tweet in test_tweets:
+        if tweet['label'] == ['yes']:
+            total_yes += 1
+        else:
+            total_no += 1
+
+    return float(true_yes)/total_yes, float(true_no)/total_no
+
+
+def printEvalFile():
+    file = open(name_eval_file, 'w')
+    file.write(getAccuracy())
+    precision_yes, precision_no = getPrecision()
+    file.write(str(precision_yes) + "  " + str(precision_no) + "\n")
+    recall_yes, recall_no = getRecall()
+    file.write(str(recall_yes) + "  " + str(recall_no) + "\n")
+    f1_yes = ((1 + 1) * precision_yes * recall_yes) / (precision_yes + recall_yes)
+    f1_no = ((1 + 1) * precision_no * recall_no) / (precision_no + recall_no)
+    file.write(str(f1_yes) + "  " + str(f1_no) + "\n")
 
 
 
@@ -127,9 +202,12 @@ def trainingPhase():
     filterVocabulary()
     getProbabilityForEachWordQ1()
 
+
 def testingPhase():
     openFileTestSet()
     testModel()
+    printTraceFile()
+    printEvalFile()
 
 
 #----MAIN----
